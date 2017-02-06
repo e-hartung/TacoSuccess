b06d2f922996d9978e672864b0964ab77f0913cb
e-hartung <elizabeth.hartung@gmail.com>
2017-02-06 16:59:20 -0500

    Add data members to CartItem class

--- TacoSuccess/Models/CartItem.cs
+++ TacoSuccess/Models/CartItem.cs
@@ -8,5 +8,15 @@ namespace TacoSuccess.Models
 {
     public class CartItem
     {
+        public Entree Entree { get; set; }
+        public int Quantity { get; set; }
+
+        public CartItem() { }
+
+        public CartItem(Entree entree, int quantity)
+        {
+            this.Entree = entree;
+            this.Quantity = quantity;
+        }
     }
 }
