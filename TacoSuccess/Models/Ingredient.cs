d8baaaba7e25036dd6d00653e298188a4073c98c
e-hartung <elizabeth.hartung@gmail.com>
2017-02-07 18:34:11 -0500

    Remove markupPrice, add markup percent constant and method to calculate markup price

--- TacoSuccess/Models/Ingredient.cs
+++ TacoSuccess/Models/Ingredient.cs
@@ -7,13 +7,20 @@ namespace TacoSuccess.Models
 {
     public class Ingredient
     {
+        public const decimal MarkupPercentage = 0.10m;
         public int ingredientsID { get; set; }
         public int vendorID { get; set; }
         public string ingredientsCode { get; set; }
         public string ingredientsName { get; set; }
         public int qtyOnHand { get; set; }
         public DateTime dateAdded { get; set; }
-        public decimal markupPrice { get; set; }
         public decimal cost { get; set; }
+
+        public decimal GetMarkupPrice()
+        {
+            decimal markupPrice;
+            markupPrice = cost * (1 + MarkupPercentage);
+            return markupPrice;
+        }
     }
 }
