3773194b2ea41c4561bf387979c6efb9018d0ec1
e-hartung <elizabeth.hartung@gmail.com>
2017-03-05 22:53:31 -0500

    Work on submit actions
    next: get order updates to db working

--- TacoSuccess/Checkout.aspx.cs
+++ TacoSuccess/Checkout.aspx.cs
@@ -4,17 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TacoSuccess.Models;
 
 namespace TacoSuccess
 {
     public partial class Checkout : System.Web.UI.Page
     {
+        tacosuccessv2Entities tse;
         public int currentMonth = DateTime.Now.Month;
         public int currentYear = DateTime.Now.Year;
 
         protected void Page_Load(object sender, EventArgs e)
         {
             UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+
+            tse = new tacosuccessv2Entities();
 
             if (!IsPostBack)
                 for (int i = 1; i <= 12; i++)
@@ -43,6 +47,43 @@ namespace TacoSuccess
             else
             {
                 // to do: add order to orders table in db
+                // put custom build in custom build table
+                // put billing info into address table
+
+                // put address info into address object, add address object to address table
+                address address = new address
+                {
+                    line1 = txtBxAddressLine1.Text,
+                    line2 = txtBxAddressLine2.Text,
+                    city = txtBxCity.Text,
+                    state = txtBxState.Text,
+                    zipCode = txtBxZip.Text,
+                    phone = txtBxPhone.Text
+                };
+
+                tse.addresses.Add(address);
+
+
+                // need to find how to add to one table to generate id for order and then get that id to add to orderdetail table
+                order order = new order
+                {
+                    cardNumber = txtBxCardNumber.Text,
+                    cardExpress = txtBxCVV2.Text
+                };
+
+                tse.orders.Add(order);
+
+
+                orderdetail orderDetail = new orderdetail
+                {
+
+                };
+
+                tse.orderdetails.Add(orderDetail);
+
+
+                // submit changes to db
+                tse.SaveChanges();
 
                 Response.Redirect("~/Confirmation.aspx");
             }
