851267ee32f0c7980a412ccaa730c42b602c3ebf
e-hartung <elizabeth.hartung@gmail.com>
2017-03-05 22:46:09 -0500

    Add IngredientInfo class

---
+++ TacoSuccess/Models/IngredientInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TacoSuccess.Models
+{
+    public class IngredientInfo
+    {
+        public int ingredientsID { get; set; }
+        public string ingredientsName { get; set; }
+        public int quantity { get; set; }
+
+        public IngredientInfo(int ingredientsID, string ingredientsName, int quantity)
+        {
+            this.ingredientsID = ingredientsID;
+            this.ingredientsName = ingredientsName;
+            this.quantity = quantity;
+        }
+    }
+}
